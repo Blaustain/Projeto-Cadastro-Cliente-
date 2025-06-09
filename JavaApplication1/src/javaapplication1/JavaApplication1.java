d3d21f7f7aed4c16ddf374167b5bac00e445dd41
Samuel Souza <103328508+Blaustain@users.noreply.github.com>
2025-06-09 19:51:46 -0300

    ProjetoCadastroCliente

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+       
+       TabsJFrame frame = new TabsJFrame();
+       frame.setVisible(true);
+    }
+
+    
+}
